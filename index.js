ee65abe71480010c5d6c86db9a2b699279a015ac
mstflotfy <mstf.lotfy@gmail.com>
2024-03-25 01:17:19 +0200

    submit 3.8*: Phonebook backend step 8

--- index.js
+++ index.js
@@ -29,8 +29,13 @@ let persons = [
   }
 ]
 
+morgan.token('body', (req, res) => {
+  return JSON.stringify(req.body)
+})
+
 app.use(express.json())
 app.use(morgan('tiny'))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 // info page
 app.get('/info', (request, response) => {
