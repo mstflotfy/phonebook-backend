3e204ab5c7ce398111574d744f4b9948203a8aeb
mstflotfy <mstf.lotfy@gmail.com>
2024-03-28 02:14:22 +0200

    submit 3.9 Phonebook backend step 9
    add cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 const generateId = () => {
@@ -33,6 +34,7 @@ morgan.token('body', (req, res) => {
   return JSON.stringify(req.body)
 })
 
+app.use(cors())
 app.use(express.json())
 app.use(morgan('tiny'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
