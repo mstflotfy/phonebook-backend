6743313daddc0b93d83ccad636b4cc23ed385693
mstflotfy <mstf.lotfy@gmail.com>
2024-03-28 03:32:19 +0200

    fix typo

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ morgan.token('body', (req, res) => {
   return JSON.stringify(req.body)
 })
 
-app.use(express.dist('dist'))
+app.use(express.static('dist'))
 app.use(cors())
 app.use(express.json())
 app.use(morgan('tiny'))
